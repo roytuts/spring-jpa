a9906389265702f311922a488a205e4627766936
roytuts <projwebinfo@gmail.com>
2021-11-19 14:56:40 +0530

    updated for spring boot 2.5.6

--- spring-data-jpa-in-clause-where-table-multiple-rows-selection-deletion/readme.rst
+++ spring-data-jpa-in-clause-where-table-multiple-rows-selection-deletion/readme.rst
@@ -1,1 +1,1 @@
-You can go through the tutorial https://www.roytuts.com/spring-data-jpa-in-clause-with-where-condition/
+Please follow the tutorial https://roytuts.com/spring-data-jpa-in-clause-with-where-condition/
---
+++ spring-data-jpa-in-clause-where-table-multiple-rows-selection-deletion/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO `product` (`id`, `name`, `code`, `price`) VALUES
+       (1, 'American Tourist', 'AMTR01', 12000),
+       (2, 'EXP Portable Hard Drive', 'USB02', 5000),
+       (3, 'Shoes', 'SH03', 1000),
+       (4, 'XP 1155 Intel Core Laptop', 'LPN4', 80000),
+       (5, 'FinePix Pro2 3D Camera', '3DCAM01', 150000),
+       (6, 'Simple Mobile', 'MB06', 3000),
+       (7, 'Luxury Ultra thin Wrist Watch', 'WristWear03', 3000),
+       (8, 'Headphone', 'HD08', 400),
+       (9, 'Test 1', 'test1', 10),
+       (10, 'Test 2', 'test2', 11),
+       (11, 'Test 3', 'test3', 12);
