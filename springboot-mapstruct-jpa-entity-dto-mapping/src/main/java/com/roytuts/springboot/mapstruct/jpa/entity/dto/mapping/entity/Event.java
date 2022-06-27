13de696ed3d73a8c41d45e45b20fe73040135213
Soumitra <projwebinfo@gmail.com>
2022-06-27 12:02:50 +0530

    Create Event.java

---
+++ springboot-mapstruct-jpa-entity-dto-mapping/src/main/java/com/roytuts/springboot/mapstruct/jpa/entity/dto/mapping/entity/Event.java
@@ -0,0 +1,93 @@
+package com.roytuts.springboot.mapstruct.jpa.entity.dto.mapping.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table
+public class Event {
+
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column
+	private String title;
+
+	@Column
+	private String url;
+
+	private String clasz;
+
+	@Column(name = "start_date")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date startDate;
+
+	@Column(name = "end_date")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date endDate;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getClasz() {
+		return clasz;
+	}
+
+	public void setClasz(String clasz) {
+		this.clasz = clasz;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Event [id=" + id + ", title=" + title + ", url=" + url + ", clasz=" + clasz + ", startDate=" + startDate
+				+ ", endDate=" + endDate + "]";
+	}
+
+}
