514ec9e84ff7f66b93ff893fa3a7a4e5bdf1373b
Soumitra <projwebinfo@gmail.com>
2022-07-08 19:05:15 +0530

    Update Config.java

--- spring-data-jpa-named-query/src/main/java/com/roytuts/spring/data/jpa/named/query/config/Config.java
+++ spring-data-jpa-named-query/src/main/java/com/roytuts/spring/data/jpa/named/query/config/Config.java
@@ -3,20 +3,19 @@ package com.roytuts.spring.data.jpa.named.query.config;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
-@Configuration
+//@Configuration
 public class Config {
 
-    @Bean
-    public DataSource dataSource() {
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.H2).addScript("classpath:sql/table.sql")
-                .addScript("classpath:sql/data.sql").build();
-        return db;
-    }
+	@Bean
+	public DataSource dataSource() {
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.H2).addScript("classpath:sql/table.sql")
+				.addScript("classpath:sql/data.sql").build();
+		return db;
+	}
 
 }
