e71bd503cf4f272e7e71cdbba7493d4463d23a7d
Soumitra <projwebinfo@gmail.com>
2022-12-14 16:57:58 +0530

    Create UserPKey.java

---
+++ springboot-data-jpa-composite-primary-key/src/main/java/com/roytuts/springboot/datajpa/composite/primarykey/entity/UserPKey.java
@@ -0,0 +1,60 @@
+package com.roytuts.springboot.datajpa.composite.primarykey.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class UserPKey implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	public UserPKey() {
+	}
+
+	public UserPKey(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getFirstName(), getLastName());
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserPKey other = (UserPKey) obj;
+
+		return Objects.equals(getFirstName(), other.getFirstName())
+				&& Objects.equals(getLastName(), other.getLastName());
+	}
+
+	@Override
+	public String toString() {
+		return "CompositePKey [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+
+}
