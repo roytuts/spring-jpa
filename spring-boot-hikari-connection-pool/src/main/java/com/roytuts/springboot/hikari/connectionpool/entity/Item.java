b9975d8f06bf8690b5705dd724a9313589bc5ebd
Soumitra <projwebinfo@gmail.com>
2023-09-22 16:50:10 +0530

    Create Item.java

---
+++ spring-boot-hikari-connection-pool/src/main/java/com/roytuts/springboot/hikari/connectionpool/entity/Item.java
@@ -0,0 +1,66 @@
+package com.roytuts.springboot.hikari.connectionpool.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table
+@Entity
+public class Item {
+
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long itemId;
+
+	@Column
+	private String itemName;
+
+	@Column
+	private String itemDesc;
+
+	@Column
+	private double itemPrice;
+
+	public long getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(long itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getItemDesc() {
+		return itemDesc;
+	}
+
+	public void setItemDesc(String itemDesc) {
+		this.itemDesc = itemDesc;
+	}
+
+	public double getItemPrice() {
+		return itemPrice;
+	}
+
+	public void setItemPrice(double itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [itemId=" + itemId + ", itemName=" + itemName + ", itemDesc=" + itemDesc + ", itemPrice="
+				+ itemPrice + "]";
+	}
+
+}
