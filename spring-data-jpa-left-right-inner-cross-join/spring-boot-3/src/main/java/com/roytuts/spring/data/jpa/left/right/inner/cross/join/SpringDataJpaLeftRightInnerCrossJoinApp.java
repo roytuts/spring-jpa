992f8fee9e411baaab3beaeddc9d97bb8ce3253a
Soumitra <projwebinfo@gmail.com>
2023-11-21 16:10:30 +0530

    Create SpringDataJpaLeftRightInnerCrossJoinApp.java

---
+++ spring-data-jpa-left-right-inner-cross-join/spring-boot-3/src/main/java/com/roytuts/spring/data/jpa/left/right/inner/cross/join/SpringDataJpaLeftRightInnerCrossJoinApp.java
@@ -0,0 +1,13 @@
+package com.roytuts.spring.data.jpa.left.right.inner.cross.join;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaLeftRightInnerCrossJoinApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaLeftRightInnerCrossJoinApp.class, args);
+	}
+
+}
