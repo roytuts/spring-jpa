fb0699786117d66538def7e8a7f021f25e9bf809
Soumitra <projwebinfo@gmail.com>
2023-11-21 16:11:31 +0530

    Create Department.java

---
+++ spring-data-jpa-left-right-inner-cross-join/spring-boot-3/src/main/java/com/roytuts/spring/data/jpa/left/right/inner/cross/join/entity/Department.java
@@ -0,0 +1,67 @@
+package com.roytuts.spring.data.jpa.left.right.inner.cross.join.entity;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "department")
+public class Department implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "description")
+	private String description;
+
+	@OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
+	private Set<Employee> employees;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Set<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(Set<Employee> employees) {
+		this.employees = employees;
+	}
+
+}
