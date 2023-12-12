29a2cc2b844a37cb3f8753012578588cbffe5dc2
Soumitra <projwebinfo@gmail.com>
2023-12-12 15:48:46 +0530

    Create Region.java

---
+++ spring-data-jpa-specification-criteria-in-clause/spring-boot-3/src/main/resources/java/com/roytuts/spring/data/jpa/specification/criteria/in/clause/entity/Region.java
@@ -0,0 +1,54 @@
+package com.roytuts.spring.data.jpa.specification.criteria.in.clause.entity;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "REGION")
+public class Region implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "ID")
+	private Integer id;
+
+	@Column(name = "NAME")
+	private String name;
+
+	@OneToMany(cascade = CascadeType.MERGE, mappedBy = "region")
+	// @OneToMany(cascade = CascadeType.ALL, mappedBy = "region")
+	private Set<Case> cases;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Case> getCases() {
+		return cases;
+	}
+
+	public void setCases(Set<Case> cases) {
+		this.cases = cases;
+	}
+
+}
