29e3f67bbf052b64e2be72181417f431da6c17d0
Soumitra <projwebinfo@gmail.com>
2023-12-12 15:51:51 +0530

    Create CaseVo.java

---
+++ spring-data-jpa-specification-criteria-in-clause/spring-boot-3/src/main/resources/java/com/roytuts/spring/data/jpa/specification/criteria/in/clause/vo/CaseVo.java
@@ -0,0 +1,33 @@
+package com.roytuts.spring.data.jpa.specification.criteria.in.clause.vo;
+
+public class CaseVo {
+
+	private int caseId;
+	private String region;
+	private String country;
+
+	public int getCaseId() {
+		return caseId;
+	}
+
+	public void setCaseId(int caseId) {
+		this.caseId = caseId;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
