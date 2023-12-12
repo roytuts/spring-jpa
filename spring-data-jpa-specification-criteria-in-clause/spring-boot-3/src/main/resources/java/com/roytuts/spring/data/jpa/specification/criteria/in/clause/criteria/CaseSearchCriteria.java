cbe050d277a407614b510ef0d55fc596a763e34d
Soumitra <projwebinfo@gmail.com>
2023-12-12 15:47:42 +0530

    Create CaseSearchCriteria.java

---
+++ spring-data-jpa-specification-criteria-in-clause/spring-boot-3/src/main/resources/java/com/roytuts/spring/data/jpa/specification/criteria/in/clause/criteria/CaseSearchCriteria.java
@@ -0,0 +1,29 @@
+package com.roytuts.spring.data.jpa.specification.criteria.in.clause.criteria;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class CaseSearchCriteria implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String country;
+	private List<Integer> regionIds;
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public List<Integer> getRegionIds() {
+		return regionIds;
+	}
+
+	public void setRegionIds(List<Integer> regionIds) {
+		this.regionIds = regionIds;
+	}
+
+}
