26f569dda0834dc012bb9a5858774957fd12a039
Soumitra <projwebinfo@gmail.com>
2023-12-15 11:00:38 +0530

    Create App.java

---
+++ spring-image-blob-image/src/main/java/com/roytuts/spring/image/blob/App.java
@@ -0,0 +1,13 @@
+package com.roytuts.spring.image.blob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
