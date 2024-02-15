af556bf0ebedaf6c32e0fd7bebe9061d29cc2836
Soumitra <projwebinfo@gmail.com>
2024-02-15 14:53:20 +0530

    Create Employee.java

---
+++ spring-data-jpa-audit/src/main/java/com/roytuts/spring/data/jpa/audit/entity/Employee.java
@@ -0,0 +1,121 @@
+package com.roytuts.spring.data.jpa.audit.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+@EntityListeners(AuditingEntityListener.class)
+public class Employee implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer empId;
+
+	// @Column(name = "name")
+	private String name;
+
+	// @Column(name = "email")
+	private String email;
+
+	// @Column(name = "address")
+	private String address;
+
+	@CreatedBy
+	@Column(name = "created_by")
+	private String createdBy;
+
+	@CreatedDate
+	@Column(name = "created_date")
+	private LocalDateTime createdDate;
+
+	@LastModifiedBy
+	@Column(name = "updated_by")
+	private String updatedBy;
+
+	@LastModifiedDate
+	@Column(name = "updated_date")
+	private LocalDateTime updatedDate;
+
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public LocalDateTime getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(LocalDateTime updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+}
