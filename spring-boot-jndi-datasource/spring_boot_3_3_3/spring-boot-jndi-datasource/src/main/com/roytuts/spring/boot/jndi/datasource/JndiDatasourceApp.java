de67599d71d873c4dab54c925a7ddf0a583a177c
Soumitra <projwebinfo@gmail.com>
2024-08-27 13:29:05 +0530

    Create JndiDatasourceApp.java

---
+++ spring-boot-jndi-datasource/spring_boot_3_3_3/spring-boot-jndi-datasource/src/main/com/roytuts/spring/boot/jndi/datasource/JndiDatasourceApp.java
@@ -0,0 +1,13 @@
+package com.roytuts.spring.boot.jndi.datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JndiDatasourceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JndiDatasourceApp.class, args);
+	}
+
+}
