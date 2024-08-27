bf4903a200fb1210de250ccadfcedd8f97488d82
Soumitra <projwebinfo@gmail.com>
2024-08-27 13:30:21 +0530

    Create DatabaseProperties.java

---
+++ spring-boot-jndi-datasource/spring_boot_3_3_3/spring-boot-jndi-datasource/src/main/com/roytuts/spring/boot/jndi/datasource/config/DatabaseProperties.java
@@ -0,0 +1,54 @@
+package com.roytuts.spring.boot.jndi.datasource.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app.datasource")
+public class DatabaseProperties {
+
+	private String driverClassName;
+	private String url;
+	private String username;
+	private String password;
+	private String jndiName;
+
+	public String getDriverClassName() {
+		return driverClassName;
+	}
+
+	public void setDriverClassName(String driverClassName) {
+		this.driverClassName = driverClassName;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getJndiName() {
+		return jndiName;
+	}
+
+	public void setJndiName(String jndiName) {
+		this.jndiName = jndiName;
+	}
+
+}
