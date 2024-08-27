b87d4f54315c1d196e3ccb075c503cab78c5f723
Soumitra <projwebinfo@gmail.com>
2024-08-27 13:31:09 +0530

    Create Company.java

---
+++ spring-boot-jndi-datasource/spring_boot_3_3_3/spring-boot-jndi-datasource/src/main/com/roytuts/spring/boot/jndi/datasource/entity/Company.java
@@ -0,0 +1,42 @@
+package com.roytuts.spring.boot.jndi.datasource.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "company")
+public class Company implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "name")
+	private String name;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
