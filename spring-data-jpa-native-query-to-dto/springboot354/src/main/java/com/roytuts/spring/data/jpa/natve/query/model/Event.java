0ece24b383b85ecb30d030e2d3f19eba60107203
Soumitra <projwebinfo@gmail.com>
2025-08-21 10:26:44 +0530

    Create Event.java

---
+++ spring-data-jpa-native-query-to-dto/springboot354/src/main/java/com/roytuts/spring/data/jpa/natve/query/model/Event.java
@@ -0,0 +1,87 @@
+package com.roytuts.spring.data.jpa.natve.query.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "event")
+public class Event {
+
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column
+	private String title;
+
+	@Column
+	private String url;
+
+	private String clasz;
+
+	@Column(name = "start_date")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date startDate;
+
+	@Column(name = "end_date")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date endDate;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getClasz() {
+		return clasz;
+	}
+
+	public void setClasz(String clasz) {
+		this.clasz = clasz;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+}
