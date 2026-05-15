de5791c3ec84f77869ca140098cff420f299ad67
Soumitra <projwebinfo@gmail.com>
2026-05-15 16:22:59 +0530

    Delete springboot-data-jpa-pagination directory

--- springboot-data-jpa-pagination/readme.md
+++
@@ -1,1 +0,0 @@
-Please follow the tutorial https://roytuts.com/pagination-with-angular-and-spring-boot/
--- springboot-data-jpa-pagination/src/main/java/com/roytuts/springboot/datajpa/pagination/repository/TutorialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.roytuts.springboot.datajpa.pagination.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.roytuts.springboot.datajpa.pagination.entity.Tutorial;
-
-public interface TutorialRepository extends JpaRepository<Tutorial, Integer> {
-
-}
